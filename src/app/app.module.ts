ed363d8e93fe280fd1d8a2826c6ce532988a2b39
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-12-03 18:20:51 -0600

    starting the setup to my first angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from '../components/header/header.component';
+import { RecipesComponent } from '../components/recipe-book/recipes/recipes.component';
+import { ShoppingListComponent } from '../components/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from '../components/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesListComponent } from '../components/recipe-book/recipe-list/recipe-list.component';
+import { RecipesItemComponent } from '../components/recipe-book/recipe-item/recipe-item.component';
+import { RecipesDetailComponent } from '../components/recipe-book/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipesListComponent,
+    RecipesItemComponent,
+    RecipesDetailComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent
   ],
   imports: [
     BrowserModule
