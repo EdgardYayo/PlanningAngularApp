0787a2173fa9f9dd6e741a825d76ed659770c053
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-12-09 18:50:33 -0600

    Developing the core of my planningApp

--- src/components/recipe-book/recipe-list/recipe-list.component.ts
+++ src/components/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
     selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from "@angular/core";
     styleUrl:'./recipe-list.component.css'
 })
 export class RecipesListComponent {
+    recipes: Recipe[] = [
+        new Recipe("Beans", "Nice Beans with Chili", "https://www.allrecipes.com/thmb/zDM9hAhVnD50qod0WhD7dZcuk_s=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/23336_FantasticBlackBeanChili_ddmfs_4x3_2150-9a545d26112c49aba786c38ef1d027d8.jpg")
+    ];
 
+    constructor() { }
+
+    ngOnInit() {}
 }
