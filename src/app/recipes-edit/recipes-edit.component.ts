6a99894669951bc81e00e55e2a8ded76e80e5e7f
EdgardYayo <allan.pazos@uabc.edu.mx>
2024-04-07 17:03:56 -0700

    Correcting the form on our page and practicing the Template and Reactive approach of making forms in Angular

--- src/app/recipes-edit/recipes-edit.component.ts
+++ src/app/recipes-edit/recipes-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../../components/recipe-book/recipe.service';
 
 @Component({
   selector: 'app-recipes-edit',
@@ -9,15 +11,39 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipesEditComponent implements OnInit {
   id: number;
   editMode = false;
+  recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
         this.id = +params['id'];
         this.editMode = params['id'] != null;
+        this.initForm();
       }
     )
   }
+
+  private initForm() {
+    let recipeName = "";
+    let recipeImage = "";
+    let recipeDescription = "";
+
+    if(this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeImage = recipe.imagePath;
+      recipeDescription = recipe.description;
+    }
+
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'imagePath': new FormControl(recipeImage),
+      'description': new FormControl(recipeDescription)
+    });
+  }
+
 }
